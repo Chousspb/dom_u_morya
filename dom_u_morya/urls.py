81701ad8b3fc73117b81c005f725489033f4240a
ALEKSANDR RIAZANTSEV <chous_spb@mail.ru>
2023-02-03 15:00:59 +0300

    third commit

--- dom_u_morya/urls.py
+++ dom_u_morya/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from houses.views import houses_list
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', houses_list)
 ]
+
+
